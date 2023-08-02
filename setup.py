d6cce88f820f792a1b585144d8bd104a4e708f2b
Brayden Moore <98822684+brayden1moore@users.noreply.github.com>
2023-08-02 16:49:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pandas',
-        'numpy',
-        'json',
-        'requests',
         'urllib',
         'tqdm',
         'names_dataset'
